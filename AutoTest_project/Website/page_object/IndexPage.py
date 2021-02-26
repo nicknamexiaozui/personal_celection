31a9bd140092249ec7d3c00e58e20491b8b2d854
nicknamexiaozui <79073164+nicknamexiaozui@users.noreply.github.com>
2021-02-26 10:18:54 +0800

    Delete AutoTest_project/Website directory

--- AutoTest_project/Website/page_object/IndexPage.py
+++
@@ -1,8 +0,0 @@
-from BasePage import *
-from selenium.webdriver.common.by import By
-class IndexPage(Page):
-    newemail_loc = (By.XPATH,'/html/body/div[1]/div/div[3]/div[2]/div[1]/div/ul/li[1]/i')
-    def type_newemael(self):
-        self.find_element(*self.newemail_loc).click()
-    def click_newmail(self):
-        self.type_newemael()
--- AutoTest_project/Website/page_object/NewmailPage.py
+++
@@ -1,8 +0,0 @@
-from BasePage import *
-from selenium.webdriver.common.by import By
-class NeweamilPage(Page):
-    quxiaofengbiefasong_loc = (By.CLASS_NAME,'bg-purple-light right_distanceone')
-    def type_quxiaofengbiefasong(self):
-        self.find_element(*self.quxiaofengbiefasong_loc).click()
-    def click_quxiaofengbiefasong(self):
-        self.type_quxiaofengbiefasong()
